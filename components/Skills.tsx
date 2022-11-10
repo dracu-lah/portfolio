d52a63e3f315e51946d6d3ea11137db163292ce8
dracu-lah <nevilnics4321@gmail.com>
2022-11-11 03:51:33 +0530

    updated skills .tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -15,7 +15,7 @@ function Skills({skills}: Props) {
     transition={{duration: 1.5 }}
     className=" flex relative flex-col text-center md:text-left xl:flex-row  xl:px-10 max-w-[2000px] h-screen justify-center  xl:space-y-0 mx-auto items-center ">
         <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
-        <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skill for currency proficency</h3>
+        <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skill for current proficency</h3>
         <div className="grid  grid-cols-4  gap-5">
             {skills?.slice(0, skills.length/2).map((skill)=>(
               <Skill key={skill._id} skill={skill}/>
